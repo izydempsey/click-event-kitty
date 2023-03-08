8d14f7097a18acf77620c36d80071b92b63b4e33
Izy Dempsey <117126818+izydempsey@users.noreply.github.com>
2023-03-07 20:41:58 -0500

    Added style changes to button using if else statements

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function() {
-    //console.log("It's working!");
+   if (cat.classList.contains("show")) {
+    //console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+   } else {
+    //console.log("no");
     cat.classList.add("show");
+    button.innerText = "Shoo, go away!"
+    button.classList.remove("disappear");
+   }
 });
